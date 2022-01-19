e93325abe93155124d3786d18397eb43f565ec85
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2022-01-19 13:52:35 +0100

    convertir entre les type en PHP

---
+++ variables/EX3.php
@@ -0,0 +1,10 @@
+<?php
+$var = true;
+$NUM = 33;
+ //Convertir un variable  booléen en entier:
+$NumC = (int) $var;
+echo $NumC;
+ //Convertir un variable entier en chaîne de caractères:
+$Chain = (string) $NUM;
+echo $Chain
+?>
